ff540c64915fddc8b920997e69c2bc3381a4394d
Cesar <cesar@caesrovich.xyz>
2026-06-04 13:35:59 +0200

    Refactor services widgets and add  custom icons

--- lib/widgets/resolved_service.dart
+++ lib/widgets/resolved_service.dart
@@ -1,6 +1,69 @@
 import 'package:aurevoir/widgets/text_divider.dart';
 import 'package:bonsoir/bonsoir.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const Map<String, IconData> serviceTypeIcons = {
+  // Web services
+  '_http': Icons.http,
+  '_https': Icons.https,
+  '_www': Icons.web,
+
+  // Mail services
+  '_smtp': Icons.email,
+  '_pop2': Icons.email,
+  '_pop3': Icons.email,
+  '_imap': Icons.email,
+  '_imap2': Icons.email,
+  '_imap3': Icons.email,
+  '_imaps': Icons.email,
+  '_submission': Icons.email,
+  '_submissions': Icons.email,
+
+  // Printers & Scanners
+  '_printer': Icons.print,
+  '_ipp': Icons.print,
+  '_ipps': Icons.print,
+  '_pdl-datastream': Icons.print,
+  '_scanner': Icons.scanner,
+  '_uscan': Icons.scanner,
+  '_uscans': Icons.scanner,
+  '_eapsp': Icons.print_sharp,
+
+  // Apple services
+  '_airplay': Icons.airplay,
+  '_airdrop': Icons.wifi_tethering,
+  '_apple-mobdev2': Icons.phone_iphone,
+  '_afpovertcp': Icons.folder_shared,
+  '_daap': Icons.music_note,
+  '_raop': Icons.speaker,
+
+  // File sharing
+  '_smb': Icons.folder_shared,
+  '_nfs': Icons.folder_shared,
+  '_ftp': Icons.folder,
+
+  // Media streaming
+  '_rtsp': Icons.videocam,
+  '_rtsp-alt': Icons.videocam,
+  '_rtsps': Icons.videocam,
+  '_rtsps-alt': Icons.videocam,
+
+  // Database services
+  '_mysql': Icons.storage,
+  '_mysql-cluster': Icons.cloud,
+  '_mysql-cm-agent': Icons.storage,
+  '_mysql-im': Icons.storage,
+  '_mysql-proxy': Icons.storage,
+  '_postgresql': Icons.storage,
+  '_pgbackrest': Icons.storage,
+  '_redis': Icons.storage,
+  '_mongodb': Icons.storage,
+
+  // Other services
+  '_ssh': Icons.computer,
+  '_mqtt': Icons.wifi,
+};
 
 class ResolvedServiceList extends StatelessWidget {
   final List<BonsoirService> services;
@@ -34,6 +97,16 @@ class ResolvedServiceRow extends StatelessWidget {
 
   const ResolvedServiceRow({super.key, required this.service});
 
+  IconData get serviceIcon {
+    final String serviceType = service.type.split('.').first;
+    return serviceTypeIcons[serviceType] ?? Icons.device_unknown;
+  }
+
+  String get hostAddresses {
+    if (service.hostAddresses.isEmpty) return 'Unknown';
+    return service.hostAddresses.join(', ');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -42,24 +115,61 @@ class ResolvedServiceRow extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 6,
           children: [
-            Text(
-              service.name,
-              style: Theme.of(context).textTheme.bodyLarge,
+            Row(
+              spacing: 6.0,
+              children: [
+                Icon(serviceIcon, size: 42),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      service.name,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(
+                      service.type,
+                      style: Theme.of(context).textTheme.labelMedium,
+                    ),
+                  ],
+                ),
+              ],
             ),
-            const SizedBox(height: 4),
-            _ServiceInformationRow(title: 'Type', value: service.type),
-            _ServiceInformationRow(title: 'Port', value: service.port.toString()),
-            _ServiceInformationRow(title: 'Hostname', value: service.hostname ?? 'Unknown'),
-            _ServiceInformationRow(title: 'Host', value: service.hostAddress ?? 'Unknown'),
+            const SizedBox(height: 8),
+            Flex(
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              spacing: 12.0,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('HOSTNAME', style: Theme.of(context).textTheme.labelMedium),
+                    Text(service.hostname ?? 'Unknown', style: Theme.of(context).textTheme.titleSmall),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('PORT', style: Theme.of(context).textTheme.labelMedium),
+                    Text(service.port.toString(), style: Theme.of(context).textTheme.titleSmall),
+                  ],
+                ),
+              ],
+            ),
+            _ServiceInformationRow(title: 'HOSTS', value: hostAddresses),
             if (service.attributes.isNotEmpty) ...[
-              const SizedBox(height: 4),
-              const Center(
-                child: Text('Attributes', style: TextStyle(fontWeight: FontWeight.bold)),
+              ExpansionTile(
+                expandedAlignment: Alignment.topLeft,
+                expandedCrossAxisAlignment: CrossAxisAlignment.start,
+                title: Text("(${service.attributes.length}) Attributes", style: Theme.of(context).textTheme.bodyMedium),
+                children: [
+                  ...service.attributes.entries.map((entry) {
+                    return _ServiceInformationRow(title: entry.key, value: entry.value);
+                  }),
+                ],
               ),
-              ...service.attributes.entries.map((entry) {
-                return _ServiceInformationRow(title: entry.key, value: entry.value);
-              }),
             ],
           ],
         ),
@@ -79,9 +189,28 @@ class _ServiceInformationRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Text('$title: ', style: const TextStyle(fontWeight: FontWeight.bold)),
-      Text(value),
-    ]);
+    return Tooltip(
+      message: "Click to copy the value",
+      waitDuration: Duration(milliseconds: 500),
+      child: Card(
+        color: Theme.of(context).colorScheme.surface,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(6),
+          onTap: () {
+            Clipboard.setData(ClipboardData(text: value));
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('$title copied to clipboard')),
+            );
+          },
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
+              child: Wrap(children: [
+                Text('$title: ', style: const TextStyle(fontWeight: FontWeight.bold)),
+                Text(value),
+              ])),
+        ),
+      ),
+    );
   }
 }
