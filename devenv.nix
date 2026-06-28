82c7bcdb769ac4366fa14d532a553c280d5768a3
Cesar <cesar@caesrovich.xyz>
2026-06-28 07:33:19 +0200

    Pin deps and create tasks

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,42 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, config, ... }:
 
+let
+  sdk = config.android.androidSdk;
+in
 {
   android = {
     enable = true;
+    tools.version = "26.1.1";
     flutter.enable = true;
+    platforms.version = [ "32" "34" "36" ];
+    buildTools.version = [ "35.0.0" ];
+    cmdLineTools.version = "11.0";
   };
 
   languages.dart.enable = true;
+
+  tasks = {
+    "aurevoir-app:doctor" = {
+      description = "Run Flutter doctor";
+      exec = "flutter doctor";
+    };
+    "aurevoir-app:clean" = {
+      description = "Clean the build artifacts";
+      exec = "flutter clean";
+    };
+    "aurevoir-app:install-dependencies" = {
+      description = "Install Flutter dependencies";
+      exec = "flutter pub get";
+    };
+    "aurevoir-app:build:linux" = {
+      description = "Build the Linux app";
+      after = [ "aurevoir-app:install-dependencies" ];
+      exec = "flutter build linux --release";
+    };
+    "aurevoir-app:build:android" = {
+      description = "Build the Android app";
+      after = [ "aurevoir-app:install-dependencies" ];
+      exec = "flutter build apk --release";
+    };
+  };
 }
